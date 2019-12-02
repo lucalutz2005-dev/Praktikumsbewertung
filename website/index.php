09a003345d21caa89c894d148b70169951b429f1
Luca Lutz <info@lucalutz.org>
2019-12-02 21:00:27 +0100

    AdminLTE

--- website/index.php
+++ website/index.php
@@ -194,7 +194,39 @@
                             <script>
                               <?php
                                 echo "var marker = L.marker([48.89203335,10.1925753437142]).addTo(Karte);"
-                              ?>
+                            $link = mysqli_connect("localhost", "luca", "LiviT2005", "praktikumsbewertung");
+                            if($link === false){
+                                die("ERROR: Could not connect. " . mysqli_connect_error());
+                            }
+                            $sql = "SELECT * FROM Firmen";
+                            if($result = mysqli_query($link, $sql)){
+                                if(mysqli_num_rows($result) > 0){
+                                    echo "<table>";
+                                        echo "<tr>";
+                                            echo "<th>id</th>";
+                                            echo "<th>first_name</th>";
+                                            echo "<th>last_name</th>";
+                                            echo "<th>email</th>";
+                                        echo "</tr>";
+                                    while($row = mysqli_fetch_array($result)){
+                                        echo "<tr>";
+                                            echo "<td>" . $row['ID'] . "</td>";
+                                            echo "<td>" . $row['first_name'] . "</td>";
+                                            echo "<td>" . $row['last_name'] . "</td>";
+                                            echo "<td>" . $row['email'] . "</td>";
+                                        echo "</tr>";
+                                    }
+                                    echo "</table>";
+                                    // Free result set
+                                    mysqli_free_result($result);
+                                } else{
+                                    echo "No records matching your query were found.";
+                                }
+                            } else{
+                                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                            }
+                            mysqli_close($link);
+                            ?>
                             </script>
                       </div>
                     </div><!-- /.card-body -->
